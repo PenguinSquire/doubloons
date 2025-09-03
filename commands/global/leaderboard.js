6a5044fca853aeb7897fa7f3df12403e802b8f88
PenguinSquire <84363175+PenguinSquire@users.noreply.github.com>
2025-09-02 20:04:49 -0400

    TETO

--- commands/global/leaderboard.js
+++ commands/global/leaderboard.js
@@ -1,10 +1,9 @@
 const { SlashCommandBuilder } = require('discord.js');
-import teto from "../../assets/teto-kasane-teto.gif"
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('leaderboard')
 		.setDescription('Shows who has the most doubloons.'),
 	async execute(interaction) {
-		await interaction.reply({content: `# You are number one!!! \n -# just trust me iyoure winning right now :)` ,  files: teto });
+		await interaction.reply({content: `# You are number one!!! \n -# just trust me iyoure winning right now :)` ,  files: ["../../assets/teto-kasane-teto.gif"] });
 	},
 };
